455f6e1b30b0041211ab71907669d8d7416a64cd
0uts1de <975side@gmail.com>
2019-06-21 23:27:19 +0900

    test

--- test.cpp
+++ test.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
+#include <iomanip>
 #include <vector>
 
 #include "aluminium/hello.hpp"
 #include "aluminium/loading.hpp"
 #include "aluminium/math/consts.hpp"
-#include "aluminium/sort/quick.hpp"
+#include "aluminium/sort.hpp"
 
 int main() {
     aluminium::hello();
     //aluminium::print_loading("hello world", 1000, 50);
-    std::cout << std::endl;
-    std::cout << aluminium::math::pi(3) << std::endl;
+    //std::cout << std::endl;
+    std::cout << atd::setprecision(10) << aluminium::math::pi(10) << std::endl;
     std::vector<int> a{13, 32, 272, 2, 2223, 22};
-    aluminium::sort::quick_sort(a.begin(), a.end());
+    aluminium::sort::quick(a.begin(), a.end());
     for (auto i : a) {
         std::cout << i << std::endl;
     }
