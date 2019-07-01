8322722ea105c2b595be17113a4b828ef4cc9f77
0uts1de <975side@gmail.com>
2019-07-01 17:59:37 +0900

    add util::erase_if

--- aluminium/aluminium.hpp
+++ aluminium/aluminium.hpp
@@ -7,6 +7,7 @@
 #include "search.hpp"
 #include "base64.hpp"
 #include "random.hpp"
+#include "util.hpp"
 
 #define ALUMINIUM
 #endif
---
+++ aluminium/util.hpp
@@ -0,0 +1,20 @@
+#ifndef ALUMINIUM_UTIL
+#define ALUMINIUM_UTIL
+
+#include <algorithm>
+#include <string>
+
+namespace aluminium {
+namespace util {
+
+// this function is same as std::erase_if() in C++20 or later.
+// use this function only if you are using C++17 or earlier.
+template <class CharT, class Traits, class Allocator, class Predicate>
+void erase_if(std::basic_string<CharT, Traits, Allocator>& c, Predicate pred) {
+    c.erase(std::remove_if(c.begin(), c.end(), pred), c.end());
+}
+
+}
+}
+
+#endif
