05c6ad14d94e28b7de069a0fc5866f766287504d
0uts1de <975side@gmail.com>
2019-08-18 11:17:06 +0900

    func name for csv

--- aluminium/csv.hpp
+++ aluminium/csv.hpp
@@ -3,13 +3,32 @@
 
 #include <fstream>
 #include <string>
+#include <vector>
 
 namespace aluminium {
 namespace csv {
 
 class csv {
-  public:
+    private:
+    public:
+        std::vector<std::vector<std::string>> load(std::ifstream file, const char sep);
+        std::vector<std::vector<std::string>> loads(std::string data);
+        int dump(std::vector<std::vector<std::string>> data, std::ofstream file, const char sep);
+        std::string dumps(std::vector<std::vector<std::string>> data);
 }
+
+std::vector<std::vector<std::string>> csv::load(std::ifstream file, const char sep) {
+}
+
+std::vector<std::vector<std::string>> loads(std::string data) {
+}
+
+int csv::dump(std::ofstream file, const char sep) {
+}
+
+std::string dumps(std::vector<std::vector<std::string>> data) {
+}
+
 
 } // namespace csv
 } // namespace aluminium
