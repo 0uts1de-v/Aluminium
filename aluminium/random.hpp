a7b238f5409c090efafeb629c42711db8538af49
0uts1de <975side@gmail.com>
2019-09-07 19:18:11 +0900

    update random::LCGs

--- aluminium/random.hpp
+++ aluminium/random.hpp
@@ -11,8 +11,8 @@ class xorShift {
     std::uint32_t x = 2463534242;
 
   public:
-    xorShift() {};
-    xorShift(std::uint32_t seed); 
+    xorShift(){};
+    xorShift(std::uint32_t seed);
     void srand(std::uint32_t seed);
     std::uint32_t rand();
     std::uint32_t operator()();
@@ -37,15 +37,21 @@ std::uint32_t xorShift::operator()() {
     return rand();
 }
 
-
 class LCGs {
   private:
     std::uint32_t x = 123456789;
 
   public:
+    LCGs(){};
+    LCGs(std::uint32_t);
     void srand(std::uint32_t seed);
     std::uint32_t rand();
+    std::uint32_t operator()();
 };
+
+LCGs::LCGs(std::uint32_t seed) {
+    srand(seed);
+}
 
 void LCGs::srand(std::uint32_t seed) {
     x = seed;
@@ -54,10 +60,12 @@ void LCGs::srand(std::uint32_t seed) {
 std::uint32_t LCGs::rand() {
     // consts by Park & Miller
     const std::uint32_t a = 48271, m = 2147483647;
-
     x = (a * x) % m;
-
     return x;
+}
+
+std::uint32_t operator()() {
+    return rand();
 }
 
 } // namespace random
